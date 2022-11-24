2158a6e8d5db5b69c811402a5b03e2b768277ec1
lei <mia.wang90@gmail.com>
2022-11-24 12:07:23 +0900

    Change user column: set uid the primary key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :posts
 
-  validates :name, presence: true
   validates :uid, presence: true, uniqueness: true
 end
--- db/migrate/20221115061251_create_users.rb
+++ db/migrate/20221115061251_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
-      t.string :name, null: false
+    create_table :users, id: false, primary_key: :uid do |t|
       t.string :uid, null: false
 
       t.timestamps
